cba375a23fc41929dabfea78e78e6aae416e144b
larrybaker2002-prog <larry.baker.2002@gmail.com>
2026-04-01 15:26:28 -0600

    Create next.config.js for Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ **/
+ const nextConfig = {
+   reactStrictMode: true,
+   // Add any other Next.js configuration options here
+ };
+ 
+ module.exports = nextConfig;
